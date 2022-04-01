a1d1f4b9151962214f04c8400605db88ae65dc1a
Hélio Rodrigues <96555695+heliorodrigues21@users.noreply.github.com>
2022-04-01 16:04:58 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+//String são cadeias de caracteres
+//Utilizamos aspas duplas""
+console.log("Imprimindo Strings com Aspas duplas");
+
+//Ou aspas simples ''
+console.log('Imprimindo Strings com Aspas Simples');
+
+//Ou a template literals ``
+console.log(`Template literals`);
+
+//Colocando expressões dentro da String
+console.log(`Soma 2+2 = ${2+2}`);
